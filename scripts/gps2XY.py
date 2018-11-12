97bfc60493b6c85f9a046882f2ae5b880dbe1dc3
SubMishMar <subodheenitr@gmail.com>
2018-11-12 14:13:02 -0600

    added imu frame

--- scripts/gps2XY.py
+++ scripts/gps2XY.py
@@ -15,19 +15,19 @@ class gpsUtilities:
 
         self.gps_sub = rospy.Subscriber('/vectornav/ins', ins, self.gps_callback)
         self.pose_pub = rospy.Publisher('/Pose', PoseStamped, queue_size=10)
-        # self.path_pub = rospy.Publisher('/Path', Path, queue_size=10)
 
         self.firstRun = True
         self.global_home = np.array([0, 0, 0])
         self.global_position = np.array([0, 0, 0])
         self.local_positionNED = np.array([0, 0, 0])
         self.local_positionENU = np.array([0, 0, 0])
-        self.br = tf.TransformBroadcaster()
+        self.brGPS = tf.TransformBroadcaster()
+        self.brLIDAR = tf.TransformBroadcaster()
         self.pose = PoseStamped()
         self.initial_bearing = 0
         self.bearing = 0
         self.init_rotn = np.identity(4)
-        # self.pose_list = deque(maxlen=1000)
+        self.listener = tf.TransformListener()
 
     def gps_to_local(self):
         (easting_home, northing_home, zone_number_home, zone_letter_home) = utm.from_latlon(self.global_home[0], self.global_home[1])
@@ -60,24 +60,28 @@ class gpsUtilities:
         trans = tf.transformations.translation_from_matrix(transformation_current)
         quatn = tf.transformations.quaternion_from_matrix(transformation_current)
 
-        self.br.sendTransform((trans[0], trans[1], trans[2]), quatn, data.header.stamp, "gps_link", "map")
+        self.brGPS.sendTransform((trans[0], trans[1], trans[2]), quatn, data.header.stamp, "/gps_link", "/map")
+        quatn_NED = tf.transformations.quaternion_from_euler(math.pi, 0, 0)
+        self.brGPS.sendTransform((0, 0, 0), quatn_NED, data.header.stamp, "/imu_link", "/gps_link")
+        try:
+        	(trans_imu_lidar, rot_imu_lidar) = self.listener.lookupTransform('/vectornav', '/velodyne', rospy.Time(0))
 
-        self.pose.header.stamp = data.header.stamp
-        # path.header.stamp = data.header.stamp
-        self.pose.header.frame_id = "map"
-        # path.header.frame_id = "map"
-        self.pose.pose.position.x = trans[0] #+ sn*self.local_positionENU[1]
-        self.pose.pose.position.y = trans[1] #+ cs*self.local_positionENU[1]
-        self.pose.pose.position.z = trans[2]
-        self.pose.pose.orientation.x = quatn[0]
-        self.pose.pose.orientation.y = quatn[1]
-        self.pose.pose.orientation.z = quatn[2]
-        self.pose.pose.orientation.w = quatn[3]
-        # self.pose_list.append(self.pose)
-        # path.poses = self.pose_list
-        self.pose_pub.publish(self.pose)
-        # self.path_pub.publish(path)
-        self.firstRun = False
+	        self.brLIDAR.sendTransform(trans_imu_lidar, rot_imu_lidar, data.header.stamp, "/lidar_link", "/gps_link");
+	        self.pose.header.stamp = data.header.stamp
+	        self.pose.header.frame_id = "map"
+	        self.pose.pose.position.x = trans[0] 
+	        self.pose.pose.position.y = trans[1] 
+	        self.pose.pose.position.z = trans[2]
+	        self.pose.pose.orientation.x = quatn[0]
+	        self.pose.pose.orientation.y = quatn[1]
+	        self.pose.pose.orientation.z = quatn[2]
+	        self.pose.pose.orientation.w = quatn[3]
+	        self.pose_pub.publish(self.pose)
+	        self.firstRun = False
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        	print('Cannot lookupTransform')
+  
         
 if __name__ == '__main__':
     rospy.init_node('gps2XY', anonymous=True)
